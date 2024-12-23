940fdf0fe85cb727502dcc91b96f893515813f90
notyour_tracy <zhanze1996@hotmail.com>
2024-12-23 15:38:37 +0800

    修改注释

--- src/thread/createthread/MyRunnable.java
+++ src/thread/createthread/MyRunnable.java
@@ -1,6 +1,11 @@
 package thread.createthread;
 
 /**
+ * 通过实现Runnable接口的方式来创建线程
+ * 1. 创建一个Runnable接口的实现类
+ * 2. 重新Runnable接口的run方法
+ * 3. 创建自定义Runnable实现类的实例，并将该实例传入到创建Thread实例的构造函数中
+ *
  * @author 詹泽
  * @since 2024/12/23 15:27
  */
